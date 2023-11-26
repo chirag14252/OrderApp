35891e8e1881db014637a4506f8ae2e27f2db998
chirag14252 <chi.chiragbhardwaj@gmail.com>
2023-11-26 22:22:33 +0530

    added update call & setItem in CartContext

--- backend/Route/orderRouter.js
+++ backend/Route/orderRouter.js
@@ -11,33 +11,38 @@ orderRoute.use(verifyToken);
 orderRoute.use(express.json());
 
 
-// orderRoute.get("/get-order",(req,res)=>{
-//     // console.log(req.user_id);
-//     return res.status(200).json({
-//         message:"get order is working fine"
-//     })
+orderRoute.get("/get-order", (req, res) => {
+  cartModal.findOne({userId : req.user_id}).then((data, err) => {
+    if(data){
+   return res.status(200).json({
+    message:"data fetched",
+    data:data
+   })
+  }
+  })
 
-// })
+  
+})
 
 
 // work to be done
 //  user_id
-orderRoute.patch("/update-order",(req,res)=>{
+orderRoute.patch("/update-order", (req, res) => {
   cartModal.findOneAndUpdate({
-    userId:req.user_id
-  },{
-   items:req.body.items
-  }).then((data,err)=>{
-   if(data){
-    return res.status(200).json({
-      message:"data updated SuccessFully"
-    })
-   }
-   if(err){
-    return res.status(400).json({
-      message:"server not updated yet"
-    })
-   }
+    userId: req.user_id
+  }, {
+    items: req.body.items
+  }).then((data, err) => {
+    if (data) {
+      return res.status(200).json({
+        message: "Cart updated SuccessFully"
+      })
+    }
+    if (err) {
+      return res.status(400).json({
+        message: "server not updated yet"
+      })
+    }
   })
 })
 
